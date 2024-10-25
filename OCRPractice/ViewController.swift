20286a6e10c0fb170d1eb0301a6a76170150de02
tshun <s.t0811@icloud.com>
2024-10-26 01:10:39 +0900

    Initial Commit

---
+++ OCRPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCRPractice
+//
+//  Created by 田島隼也 on 2024/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
